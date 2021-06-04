65444f6f714f97a071e06310c0ec440eea125cfe
schouwlars <lars@schouw.com>
2021-06-04 09:36:06 +0900

    Add XL UDF subscribe and improve tick

--- cpp/src/adaptors/xll/TickStream.cpp
+++ cpp/src/adaptors/xll/TickStream.cpp
@@ -4,8 +4,27 @@
 #include <spdlog/async.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/rotating_file_sink.h>
+#include <codecvt>
 
 using namespace xll;
+
+using xcstr = traits<XLOPERX>::xcstr;
+
+std::wstring s2ws(const std::string& str)
+{
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.from_bytes(str);
+}
+
+std::string ws2s(const std::wstring& wstr)
+{
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.to_bytes(wstr);
+}
 
 int XLL_ERROR(const char* s, bool b)
 {
@@ -37,19 +56,55 @@ Auto<Open> xai_open([]() {
 		spdlog::register_logger(logger);
 	}
 
-	StreamingMarketData::getInstance().SecID("BTC/USD");
-	StreamingMarketData::getInstance().start();
 	return TRUE;
 });
 
 Auto<Close> xai_close([]() {
-	StreamingMarketData::getInstance().stop();  // shutdown the market data stream.
-	spdlog::drop_all();
+	//StreamingMarketData::getInstance().stop();  // shutdown the market data stream.
+	//spdlog::drop_all();
 	return TRUE;
 });
 
+AddIn xai_subscribe(
+	Function(XLL_LPOPER, "xll_subscribe", "SUBSCRIBE")
+	.Arguments({
+		Arg(XLL_CSTRING, "SecCode", "The security code", "")
+		})
+	.FunctionHelp("Subscribe to tick data.")
+	.Category("CRYPTO-MARKET")
+	.HelpTopic("")
+);
+LPOPER WINAPI xll_subscribe(xcstr seccode)
+{
+#pragma XLLEXPORT
+	static OPER result;
+	static auto firstCall = true;
+
+	try {
+		std::string sc = ws2s(seccode);
+		if (firstCall)
+		{
+			StreamingMarketData::getInstance().SecID(sc);
+			StreamingMarketData::getInstance().start();
+			firstCall = false;
+		}
+		else
+		{		
+			StreamingMarketData::getInstance().Subscribe(sc);
+		}
+		result = "OK";	// We will receive an async message with the result of the message send to the exchange server. 
+						// Maybe have a future wait for the message to arrive with the result of this request.
+						// At the moment it will be send to the log
+	}
+	catch (std::exception ex) {
+		result = ex.what();
+	}
+
+	return &result;
+}
+
 AddIn xai_tick(
-	Function(XLL_DOUBLE, "xll_tick", "TICK")
+	Function(XLL_LPOPER, "xll_tick", "TICK")
 	.Arguments({
 		Arg(XLL_CSTRING, "SecCode", "The security code", "")
 	})
@@ -57,11 +112,19 @@ AddIn xai_tick(
 	.Category("CRYPTO-MARKET")
 	.HelpTopic("")
 );
-double WINAPI xll_tick()
+LPOPER WINAPI xll_tick(xcstr seccode)
 {	
-#pragma XLLEXPORT	
+#pragma XLLEXPORT
+	static OPER result;
 
-	Tick t = StreamingMarketData::getInstance().getTick("BTC/USD");
-	return t.last;
-	// return 5.0;
+	try {		
+		std::string sc = ws2s(seccode);
+		Tick t = StreamingMarketData::getInstance().getTick(sc);
+		result = (t.bid+t.ask)/2.0;		
+	}
+	catch (std::exception ex) {
+		result = ex.what();
+	}
+
+	return &result;
 }
